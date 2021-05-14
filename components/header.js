f24940ad3542e6555c3f3e40e7d1745932c528f2
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-05-15 01:08:06 +0530

    add : cart count on header

--- components/header.js
+++ components/header.js
@@ -3,8 +3,9 @@ import Link from 'next/link'
 import { Navbar } from 'react-bootstrap';
 import { FaUserCircle, FaShoppingCart } from 'react-icons/fa';
 import { useRouter } from 'next/router'
+import { connect } from 'react-redux';
 
-const header = () => {
+const header = ({cart_count}) => {
 
     const router = useRouter()
     const { shop_slug } = router.query;
@@ -12,13 +13,13 @@ const header = () => {
     console.log(router);
 
     const goToHome = () => {
-        if(shop_slug){
+        if (shop_slug) {
             router.push(`/${shop_slug}`)
         }
-        else{
+        else {
             router.back()
         }
-        
+
     }
     return (
         <Navbar className="fixed_header">
@@ -26,8 +27,13 @@ const header = () => {
                 <Navbar.Brand > <div className="brand h1" onClick={goToHome} role="button" >Shop Name</div> </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text className="m-2">
-                        <Link href="/cart" className="nav-link" ><FaShoppingCart size="40" role="button" /></Link>
+                    <Navbar.Text className="m-2" role="button">
+                        <Link href="/cart" className="nav-link" >
+                            <>
+                            <FaShoppingCart size="40" />
+                            <span class="badge badge-danger cart_count">{cart_count}</span>
+                            </>
+                            </Link>
                     </Navbar.Text>
                     <Navbar.Text className="m-2">
                         <Link href="/profile" className="nav-link"><FaUserCircle size="40" role="button" /></Link>
@@ -38,4 +44,11 @@ const header = () => {
     );
 };
 
-export default header;
+const mapStateToProps = (state, ownProps) => {
+    return {
+      cart_count: state.cart.data.length,
+    }
+  }
+  
+
+export default connect(mapStateToProps)(header);
--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,14 +1,46 @@
-export default function Cart() {
+import { useRouter } from 'next/router'
+import { FaRegArrowAltCircleLeft } from 'react-icons/fa';
+import { fetchcart } from '@/redux/index'
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+function Cart({ fetchcart, cart }) {
+
+  const router = useRouter()
+  const goBack = () => {
+    router.back()
+  }
+
+  useEffect(() => {
+    fetchcart();
+  }, []);
+
+  console.log(cart);
+
   return (
     <>
       <h3 className="header_h3">
         <div className="container ">
-        
-        </div>
+          <button type="button" onClick={goBack} className="btn"> <FaRegArrowAltCircleLeft size="35" /> </button> Back
+      </div>
       </h3>
-      <div className="pt-200">
-          Design Your Cart Here
+      <div className="row pt-200">
+
       </div>
     </>
   )
 }
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    cart: state.cart,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchcart: () => dispatch(fetchcart())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);

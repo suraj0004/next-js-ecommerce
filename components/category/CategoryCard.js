787b86b9a66dee1f0cb8fd2fca7e25b85dc3352b
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-05-16 16:34:01 +0530

    update : category card

--- components/category/CategoryCard.js
+++ components/category/CategoryCard.js
@@ -12,11 +12,11 @@ function CategoryCard(props) {
         <Link href={`${shop_slug}/${category.slug}`}>
             <div className="card bg-dark text-white shadow-lg p-3 mb-5 bg-white rounded category">
                 <img
-                    src={category.image}
+                    src={category.thumbnail}
                     className="card-img-top img-fluid"
                 />
                 <div className="card-img-overlay">
-                    <h5 className="card-title ">{category.name}</h5>
+                    <h5 className="card-title category_label">{category.name} ({category.product_count}) </h5>
                 </div>
             </div>
         </Link>
